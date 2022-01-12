4bc20beeac582b7c25d3209d6b7833f0fe69bbb4
Madisyn Vail <virtualmadi3@gmail.com>
2022-01-12 10:20:18 -0600

    commit tree baf90b9b06e4970b6a85480f2be4a979113de562

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickup) {
+    if (pickup > 42) {
+        return pickup - 42;
+    }
+    else {
+        return 42 - pickup;
+    }
+}
+
+function distanceFromHqInFeet(pickup) {
+    let blocks = distanceFromHqInBlocks(pickup);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    }
+    else {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+        if (feet <= 400 ) {
+            return 0;
+        }
+        else if (feet <= 2000) {
+            return (feet - 400) * .02;
+        }
+        else if (feet < 2500) {
+            return 25;
+        }
+        else {
+            return 'cannot travel that far';
+        }
+}
